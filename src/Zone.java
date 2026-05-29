3fa58a6de069ab754ca6361a7303d252db81c16f
Ege Kuru <egekuru2006@gmail.com>
2026-05-29 18:24:37 +0300

    Added variables for "Zone" Classes.

--- src/Zone.java
+++ src/Zone.java
@@ -1,7 +1,21 @@
 public class Zone extends Cell
 {
+    protected int level;    // level
+
+    protected int receivedElectricity;      //Utility
+    protected int receivedWater;
+    protected int receivedInternet;
+
+    protected boolean hasSecurity;      //Service
+    protected boolean hasHealth;
+    protected boolean hasEducation;
+
+    protected int receivedPopulation;       //Population
+
     public Zone(int coordinateX, int coordinateY)
     {
         super(coordinateX,coordinateY);
     }
+
+
 }
